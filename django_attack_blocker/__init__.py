0e0dbd1de8c77c7cb69c319501a7f565a93b7f6e
benab04 <benabrahambiju@gmail.com>
2025-04-17 03:13:32 +0530

    update version

--- django_attack_blocker/__init__.py
+++ django_attack_blocker/__init__.py
@@ -1,4 +1,4 @@
 from .core import MLIPBlocker, block_if_malicious, with_ip_blocking
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 __all__ = ["MLIPBlocker", "block_if_malicious", "with_ip_blocking"]
